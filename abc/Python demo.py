ffbe0426636bdcfc95b3671985eabf15043724b7
manikanta <alla.manikanta999@gmail.com>
2023-11-09 12:50:11 +0530

    initial commit

---
+++ abc/Python demo.py
@@ -0,0 +1,10 @@
+def arithmetics(a,b): 
+    print("performing arithmetic operations for :",a,b);
+    sum =a+b;
+    print("sum",sum);
+    diff=a-b;
+    print("diff",diff)
+
+a=10;
+b=5;
+arithmetics(a,b)\
